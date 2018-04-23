b7a1272dce5773f49b373e8dec7519e0c7813da0
necatisozer <32370872+necatisozer@users.noreply.github.com>
2018-04-23 19:28:13 +0300

    Add project files.

---
+++ MyMovieCollection/Utils/ImageConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyMovieCollection.Utils
+{
+    public class ImageConfiguration
+    {
+        public static string secure_base_url = "https://image.tmdb.org/t/p/";
+        public static List<string> poster_sizes = new List<string>(new string[] { "w92", "w154", "w185", "w342", "w500", "w780", "original" });
+        public static string imdb_base_title_url = "https://www.imdb.com/title/";
+    }
+}
